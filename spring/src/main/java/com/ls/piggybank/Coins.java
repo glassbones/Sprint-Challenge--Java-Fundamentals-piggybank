24f829f31016bad936612ed91c059a30ed61f86f
glassbones <glassbonesofficial@gmail.com>
2020-07-10 11:07:33 -0500

    setup

---
+++ spring/src/main/java/com/ls/piggybank/Coins.java
@@ -0,0 +1,77 @@
+package com.ls.piggybank;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Coins {
+
+    //fields
+    private static final AtomicLong counter = new AtomicLong();
+    private long coinid;
+    private String name;
+    private String namepural;
+    private double value;
+    private double quantity;
+
+    public Coins(String name,
+                 String namepural,
+                 double value,
+                 double quantity)
+    {
+        this.coinid = counter.incrementAndGet(); //maxId++
+        this.name = name;
+        this.namepural = namepural;
+        this.value = value;
+        this.quantity = quantity;
+    }
+
+    public long getCoinid() {
+        return coinid;
+    }
+
+    public void setCoinid(long coinid) {
+        this.coinid = coinid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNamepural() {
+        return namepural;
+    }
+
+    public void setNamepural(String namepural) {
+        this.namepural = namepural;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Coins{" +
+                "coinid=" + coinid +
+                ", name='" + name + '\'' +
+                ", namepural='" + namepural + '\'' +
+                ", value=" + value +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
---
+++ spring/src/main/java/com/ls/piggybank/totalCoins.java
@@ -0,0 +1,18 @@
+package com.ls.piggybank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class totalCoins {
+
+    public List<Coins> totalCoins = new ArrayList<>();
+
+    public totalCoins(){
+
+        totalCoins.Coins(new Employee("Steve","Green",45000,true,1,1));
+        totalCoins.Coins(new Employee("Sam","Ford",80000,true,1,1));
+        totalCoins.Coins(new Employee("John","Jones",75000,true,1,1));
+
+    }
+
+}
